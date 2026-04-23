d5d49e55139a34986f9cadc8740737f6271df108
shiningsuite-alt <shiningsuite@gmail.com>
2026-04-23 10:58:15 +0100

    Add files via upload

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import general_functions
 from tabulate import tabulate
 from pathlib import Path
-from items_file import (
+from product import (
     create_product,
     update_product,
     delete_product,
@@ -18,7 +18,7 @@ from orders_module import (
     sales,
     default_sales
 )
-from schedule import (
+from sellers import (
     create_seller,
     read_sellers,
     update_seller,
@@ -101,16 +101,16 @@ def main():
                                         else:
                                             print("No preexisting item classes")
                                             general_functions.pause()
-                                return_code=create_product(item_name, item_price, item_quantity, item_class)
+                                return_code, return_name=create_product(item_name, item_price, item_quantity, item_class)
                                 if return_code[0] == 200:
-                                    print("Item added successfully")
+                                    print(return_name+" added successfully")
                                 else:
                                     print("Item not added")
                             elif make_choice == 2:
                                 item_id=general_functions.validation_check()
-                                return_code=read_product_by_id(item_id)
+                                return_code, return_item_values, return_item_quantity=read_product_by_id(item_id)
                                 if return_code[0]==200:
-                                    print("Item found successfully")
+                                    print(return_item_values+ " quantity:"+ str(return_item_quantity))
                                 else:
                                     print("Item not found")
                             elif make_choice == 3:
@@ -127,20 +127,20 @@ def main():
                                     new_price=general_functions.validation_check_float()
                                     print("Input new quantity")
                                     new_quantity=general_functions.validation_check()
-                                    return_code=update_product(item_id,new_name,new_class,new_price,new_quantity)
+                                    return_code, return_item_values, return_item_quantity=update_product(item_id,new_name,new_class,new_price,new_quantity)
                                 else:
                                     return_code= 204, "no content"
                                 if return_code[0] == 200:
-                                    print("Item changed successfully")
+                                    print(return_item_values+ " quantity:"+ str(return_item_quantity))
                                 elif return_code[0] == 204:
                                     print("No class to choose from")
                                 else:
                                     print("Item not modified")
                             elif make_choice == 4:
                                 item_id = general_functions.validation_check()
-                                return_code=delete_product(item_id)
+                                return_code, return_name=delete_product(item_id)
                                 if return_code[0]==200:
-                                    print("Item removed successfully")
+                                    print(return_name+" removed successfully")
                                 else:
                                     print("Item not found")
                             else:
@@ -255,16 +255,16 @@ def main():
                     print("Select time")
                     time_select = general_functions.validation_check_2(9)
                     time = schedule["list_of_time"][time_select - 1]
-                    return_code=create_seller(name, product_type, extra_description, day, time)
+                    return_code, return_seller_values=create_seller(name, product_type, extra_description, day, time)
                     if return_code[0]==200:
-                        print("Seller added successfully")
+                        print(return_seller_values)
                     general_functions.pause()
                 elif make_choice == 2:
                     print("Write seller id")
                     seller_id = general_functions.validation_check()
-                    return_code=read_sellers(seller_id)
+                    return_code,return_seller_values=read_sellers(seller_id)
                     if return_code[0]==200:
-                        print("Seller found successfully")
+                        print(return_seller_values)
                     else:
                         print("Seller not found")
                     general_functions.pause()
@@ -289,20 +289,20 @@ def main():
                     print("Select time")
                     time_select = general_functions.validation_check_2(9)
                     time = schedule["list_of_time"][time_select - 1]
-                    return_code= update_seller(input_id,name, product_type, extra_description, day, time)
+                    return_code, return_seller_values= update_seller(input_id,name, product_type, extra_description, day, time)
                     if return_code[0]==200:
-                        print("Seller added successfully")
+                        print(return_seller_values)
                     else:
                         print("Id not found")
                     general_functions.pause()
                 elif make_choice == 4:
                     print("Write seller id")
                     seller_id=general_functions.validation_check()
-                    return_code=delete_sellers(seller_id)
+                    return_code, return_name=delete_sellers(seller_id)
                     if return_code[0]==404:
                         print("sellers not found")
                     else:
-                        print("Seller deleted successfully")
+                        print(return_name+" deleted successfully")
                     general_functions.pause()
                 else:
                     print("Leaving...")
